1b60a792bd30843e857d15fa3e4110f198604381
The Phantom Technology -Advanced Pentest <155259033+The-Phantom-Technology@users.noreply.github.com>
2024-12-14 11:31:07 +0800

    更新

---
+++ PHOSBUILD.sh
@@ -0,0 +1,27 @@
+rm -rf /opt/PHANTX/mbtget
+
+cd /opt/PHANTX
+
+git clone https://github.com/vanhauser-thc/thc-ipv6
+
+cd /opt/PHANTX/mbtget
+
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+perl Makefile.PL
+
+make
+
+make install
+
+chmod -R 755 /opt/PHANTX/bin
+rm -rf /opt/PHOS/tool/mbtget
+rm -rf /opt/PHOS/tool/mbtget
